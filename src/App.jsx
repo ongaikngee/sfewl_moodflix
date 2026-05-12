220c1324f0c4bdd8359fa9d2bf439f7ae1368ec9
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-12 10:54:41 +0800

    Add new assets and Search component
    - Added logo.png and no-movie.png images to the public directory.
    - Introduced search.svg and star.svg icons for UI enhancements.
    - Created a new Search component in the src/components directory to handle movie searches, including an input field and search icon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return (<h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>)
-   
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
